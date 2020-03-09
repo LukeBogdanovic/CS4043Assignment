7907791af81b5994dd3a1d155e51e434c804ddb6
BOGDANOVIC LUKE <c19232691@cs305-02.CSIS>
2020-03-09 14:00:11 +0000

    Version 1.0.0

--- CS4043ProgrammingAssignment/main.lua
+++ CS4043ProgrammingAssignment/main.lua
@@ -4,4 +4,21 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local lives = 3
+local died = false
+
+local  livesText
+
+local backGroup = display.newGroup()
+local mainGroup = display.newGroup()
+local uiGroup = display.newGroup()
+
+--local background = display.newImageRect(backGroup,"",,)
+--background.x = display.contentCenterX
+--background.y = display.contentCenterY
+
+livesText	= display.newText( uiGroup,"Lives: "..lives,200,80,native.systemFont,36 )
+
+local function updateText()
+  livesText.text = "Lives: "..lives
+end
