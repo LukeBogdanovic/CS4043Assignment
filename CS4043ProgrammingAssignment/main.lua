1f9acbef15ac2e5bb3f562b90e2ae593b11f00cf
LukeBogdanovic <58079356+LukeBogdanovic@users.noreply.github.com>
2020-03-12 21:47:18 +0000

    version1.0.0

--- CS4043ProgrammingAssignment/main.lua
+++ CS4043ProgrammingAssignment/main.lua
@@ -6,10 +6,8 @@
 
 local composer = require( "composer" )
 
-display.setStatusBar( display.HiddenStatusBar )
+display.setStatusBar( display.hiddenStatusBar )
 
 math.randomseed( os.time() )
 
-local function gotoMenu()
-  composer.gotoScene( "menu" )
-end
+composer.gotoScene( "menu" )
