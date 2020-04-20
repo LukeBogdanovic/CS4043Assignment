4c21530005311fd88872e180fe92585c68b1934f
LukeBogdanovic <58079356+LukeBogdanovic@users.noreply.github.com>
2020-04-20 20:41:49 +0100

    removes

--- CS4043ProgrammingAssignment/config.lua
+++ CS4043ProgrammingAssignment/config.lua
@@ -8,16 +8,8 @@ application =
 	content =
 	{
 		width = 1920,
-		height = 1080, 
+		height = 1080,
 		scale = "letterbox",
 		fps = 60,
-
-		--[[
-		imageSuffix =
-		{
-			    ["@2x"] = 2,
-			    ["@4x"] = 4,
-		},
-		--]]
 	},
 }
