dbf41cae86649450f80f5509a0b7b2757c3e0ccb
LukeBogdanovic <58079356+LukeBogdanovic@users.noreply.github.com>
2020-04-21 01:58:32 +0100

    Menu updates

--- CS4043ProgrammingAssignment/menu.lua
+++ CS4043ProgrammingAssignment/menu.lua
@@ -4,30 +4,45 @@ local composer = require( "composer" )
 local scene = composer.newScene()
 
 local function goToGame()
-  composer.gotoScene( "game" )
+  composer.gotoScene( "Level1", {time=800, effect="crossFade"} )
 end
 
 local function goToOptions()
-  composer.gotoScene("options")
+  composer.gotoScene("options",{time=800,effect="crossFade"})
 end
+
+local backGroup
+local uiGroup
 
 function scene:create( event )
   local sceneGroup = self.view
 
-  local background = display.newImageRect( sceneGroup , "background1.png" , 1920 , 1080 )
+  backGroup = display.newGroup()
+  sceneGroup:insert( backGroup )
+
+  uiGroup = display.newGroup()
+  sceneGroup:insert( uiGroup )
+
+  local background = display.newImageRect( backGroup , "Level1Background.png" , 1920 , 1080 )
   background.x = display.contentCenterX
   background.y  = display.contentCenterY
 
-  local title = display.newImageRect( sceneGroup, "title.png", 500, 200 )
+  local title = display.newImageRect( backGroup, "title.png", 600, 300 )
   title.x = display.contentCenterX
   title.y = 200
 
-  local playButton = display.newText( sceneGroup, "play", display.contentCenterX, 700 , native.systemFont,44 )
-  playButton:setFillColor(0.82,0.86,1)
+  local playButton = display.newImageRect( uiGroup, "Button.png",  600, 400 )
+  playButton.x = display.contentCenterX - 25
+  playButton.y = 450
 
-  local optionsButton = display.newText( sceneGroup, "options" , display.contentCenterX , 810 ,native.systemFont , 44 )
-  optionsButton:setFillColor(0.75,0.78,1)
+  local optionsButton = display.newImageRect( uiGroup, "Button.png", 600, 400 )
+  optionsButton.x = display.contentCenterX - 25
+  optionsButton.y = 750
 
+  local playText = display.newText( uiGroup,"Play",display.contentCenterX-25,450,"Font.ttf",108 )
+
+  local optionsText = display.newText( uiGroup,"Options",display.contentCenterX-25,750,"Font.ttf",108 )
+  
   playButton:addEventListener("tap", goToGame)
   optionsButton:addEventListener("tap", goToOptions)
 end
--- CS4043ProgrammingAssignment/options.lua
+++ CS4043ProgrammingAssignment/options.lua
@@ -8,13 +8,39 @@ local function backToMain()
   composer.gotoScene( "menu" )
 end
 
+local backGroup
+local uiGroup
+
 function scene:create( event )
   local sceneGroup = self.view
+
+  backGroup = display.newGroup()
+  sceneGroup:insert(backGroup)
+
+  uiGroup = display.newGroup()
+  sceneGroup:insert(uiGroup)
 
   local background = display.newImageRect( sceneGroup, "background1.png",1920,1080  )
   background.x = display.contentCenterX
   background.y = display.contentCenterY
 
-  local backButton = display.newRect( sceneGroup , display.contentCenterX , 540, 220, 80 )
+  local backButton = display.newImageRect( uiGroup , display.contentCenterX , 540, 220, 80 )
+  backButton.x = display.contentCenterX
+  backButton.y = 900
 
-  local backButtonText = display.newText( sceneGroup ,display.contentCenterX,  )
+  local backButtonText = display.newText( uiGroup ,"Back To Main",display.contentCenterX-25,900,"Font.ttf",108 )
+end
+
+function scene:show( event )
+
+  local sceneGroup = self.view
+  local phase = event.phase
+
+  if ( phase == "will" ) then
+
+  elseif ( phase == "did" ) then
+
+  end
+end
+
+return scene 
