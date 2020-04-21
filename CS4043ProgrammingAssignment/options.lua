428145da2b5b98707f992bdf6b7076ee8eec7303
LukeBogdanovic <58079356+LukeBogdanovic@users.noreply.github.com>
2020-04-21 14:08:50 +0100

    options displaying

--- CS4043ProgrammingAssignment/options.lua
+++ CS4043ProgrammingAssignment/options.lua
@@ -1,10 +1,10 @@
 --options.lua
-local composer = new require("composer")
+local composer = require( "composer" )
 
 local scene = composer.newScene()
 
 local function backToMain()
-  composer.gotoScene( "menu" )
+  composer.gotoScene( "menu",{time=800,effect="crossFade"} )
 end
 
 local backGroup
@@ -23,11 +23,13 @@ function scene:create( event )
   background.x = display.contentCenterX
   background.y = display.contentCenterY
 
-  local backButton = display.newImageRect( uiGroup , display.contentCenterX , 540, 220, 80 )
-  backButton.x = display.contentCenterX
+  local backButton = display.newImageRect( uiGroup ,"Button.png", 600, 400 )
+  backButton.x = display.contentCenterX-25
   backButton.y = 900
 
   local backButtonText = display.newText( uiGroup ,"Back To Main",display.contentCenterX-25,900,"Font.ttf",108 )
+
+  backButton:addEventListener("tap", backToMain)
 end
 
 function scene:show( event )
@@ -59,5 +61,10 @@ function scene:destroy( event )
   local sceneGroup = self.view
 
 end
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
 
 return scene
